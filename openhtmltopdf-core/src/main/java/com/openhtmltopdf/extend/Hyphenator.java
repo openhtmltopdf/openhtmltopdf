c18c29307141c3054353d804218e8a3939bfd84d
Marcel Rentmeister <m.rentmeister@babiel.com>
2024-07-01 15:19:15 +0200

    Add JavaDoc for hyphantion function

--- openhtmltopdf-core/src/main/java/com/openhtmltopdf/extend/Hyphenator.java
+++ openhtmltopdf-core/src/main/java/com/openhtmltopdf/extend/Hyphenator.java
@@ -5,5 +5,12 @@ package com.openhtmltopdf.extend;
  */
 @FunctionalInterface
 public interface Hyphenator {
+    /**
+     * Hyphenates a given String by inserting soft-hyphens
+     * at desired hyphenation points and returns the result.
+     *
+     * @param text String to hyphenate
+     * @return with soft-hyphens hyphenated String
+     */
     String hyphenateText(String text);
 }
